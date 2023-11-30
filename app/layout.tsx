e497eb3d5172a01913a22f3b06c81f764bccae7b
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-11-30 11:06:32 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,22 +6,25 @@ import Footer from "./components/layout/footer";
 import Header from "./components/layout/header";
 import GlobalPovider from "@/sdk/provider";
 
-// export const metadata: Metadata = {
-//   title: '...',
+export const metadata: Metadata = {
+  title: "DHLimited Home - Global Logistics and International Shipping.",
+  description:
+    "DHLimited is the global leader in the logistics industry. Specializing in international shipping, courier services and transportation.",
+  icons: [{ url: "/favicon.png", href: "/favicon.png" }],
+};
+
+// export async function generateMetadata() {
+//   const IPData = await fetch(" https://ipapi.co/json/").then((response) =>
+//     response.json()
+//   );
+
+//   return {
+//     title: `DHLimited Home - Global Logistics and Shipping ${IPData?.country_name}`,
+//     description:
+//       "DHLimited is the global leader in the logistics industry. Specializing in international shipping, courier services and transportation.",
+//     icons: [{ url: "/favicon.png", href: "/favicon.png" }],
+//   };
 // }
-
-export async function generateMetadata() {
-  const IPData = await fetch(" https://ipapi.co/json/").then((response) =>
-    response.json()
-  );
-
-  return {
-    title: `DHLimited Home - Global Logistics and Shipping ${IPData?.country_name}`,
-    description:
-      "DHLimited is the global leader in the logistics industry. Specializing in international shipping, courier services and transportation.",
-    icons: [{ url: "/favicon.png", href: "/favicon.png" }],
-  };
-}
 
 export default function RootLayout({
   children,
